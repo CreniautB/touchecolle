36fbf98ab0b65d8bd11861f749f159622300190b
Creniaut Benjamin <creniaut.benjamin@gmail.com>
2021-08-27 14:54:12 +0200

    ajout logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './component/homePage/homePage';
+import './component/homePage/homePage.css'
 
 
 function App() {
   return (
     <div className="homePage">
-        <img src='img/logo.png' alt='logo' width='120px' ></img>
+        
+        <img src="img/logo.png" alt='logo' width='120'></img>
+        <br/>
+        <br/>
+        <a href="https://www.calldsk.fr" target="_blank" className="buttonLink" >www.calldsk.fr</a>
+       
         <HomePage />
     </div>
   );
